cb8ed839d146f5335fd42ba49fa0284b35346380
Tomoe <osmanthus31172@gmail.com>
2025-04-25 22:01:34 +0900

    iosズームイン対応、お絵描きページ装飾

--- src/app/_components/CatWalk.jsx
+++ src/app/_components/CatWalk.jsx
@@ -13,13 +13,13 @@ export const CatWalk = () => {
         alt="character"
         initial={{ x: "500%" }}
         animate={{
-          x: "-70%",
-          y: [0, -10, 0, 10, 0], // 上下にゆらゆら
-          rotate: [0, 10, 45, 10, 0], // 少し傾く
+          x: "-130%",
+          y: [0, 10, 0, 10, 0], // 上下にゆらゆら
+          rotate: [15, 24, 15], // 少し傾く
         }}
-        className="absolute bottom-50 w-2 h-[100px] w-auto"
+        className="absolute top-7 w-2 h-[100px] w-auto"
         transition={{
-          x: { duration: 11, repeat: Infinity, ease: "linear" },
+          x: { duration: 9, repeat: Infinity, ease: "linear", delay: 0 },
           y: {
             duration: 2,
             repeat: Infinity,
@@ -39,10 +39,26 @@ export const CatWalk = () => {
         width={150}
         height={150}
         alt="character"
-        initial={{ x: "1000%" }}
-        animate={{ x: "-70%" }}
-        className="absolute bottom-50 h-24 w-auto "
-        transition={{ duration: 13, repeat: Infinity, ease: "linear" }}
+        initial={{ x: "800%" }}
+        animate={{
+          x: "-130%",
+          scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1],
+        }}
+        className="absolute top-7 h-24 w-auto "
+        transition={{ duration: 9, repeat: Infinity, ease: "linear", delay: 2 }}
+      />
+      <motion.img
+        src="/top-img/mokumoku.png"
+        width={150}
+        height={150}
+        alt="character"
+        initial={{ x: "800%", scale: 0.8 }}
+        animate={{
+          x: "-130%",
+          rotate: [0, 360, 0, 360],
+        }}
+        className="absolute top-7 h-24 w-auto "
+        transition={{ duration: 9, repeat: Infinity, ease: "linear", delay: 4 }}
       />
     </div>
   );
